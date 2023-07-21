3ef1f75c337adee422ad3441a5bfe34d3d377fcb
Elif Nur <138946342+elifnuryn@users.noreply.github.com>
2023-07-21 16:25:24 +0300

    Add files via upload

---
+++ Büyük harfe çevirme.py
@@ -0,0 +1,7 @@
+def harfleribuyut():
+    kelime=str(input("Bir kelime girin:"))
+    kelimeninyeniyazılısı=kelime.upper()
+    print("Girdiğiniz kelimenin büyük harflerle yazılışı:",kelimeninyeniyazılısı)
+
+if __name__=="__main__":
+    harfleribuyut()
